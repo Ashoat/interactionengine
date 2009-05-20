3c5a8835aff283292e3332ba34d2371e38203a57
ashoat <ashoat@d5ddfdca-4257-11de-952f-710615d9d0d5>
2009-05-20 17:07:32 +0000

    Changed GameField to be compatible with new changes in InteractionEngine.

--- TestNetworkGame/GameWorld/Logic/GameField.cs
+++ TestNetworkGame/GameWorld/Logic/GameField.cs
@@ -12,81 +12,31 @@ namespace TestNetworkGame.Logic {
         #region Constructors
 
         /// <summary>
-        /// This helper methods creates all of this GameObject's fields in constant order.
+        /// This helper method creates all of this GameObject's fields in constant order.
         /// </summary>
-        private void makeFields() {
-
+        public override void makeFields() {
         }
 
-        #region Client
-
-        // The classHash, a unique identifying string for the class. 
-        // Used for the factory methods called when the client receives a CREATE_OBJECT update from the server computer.
-        internal const string classHash = "GameField";
-
         /// <summary>
-        /// Returns the class hash. 
+        /// All GameObjects need a parameterless constructor for calling by GameObject.createGameObject() and GameObject.createFromUpdate().
+        /// NEVER CALL THIS! This constructor is exclusively for use by the InteractionEngine. If anyone else calls it things will break.
         /// </summary>
-        /// <returns>The class hash. Do we really have to tell you everything twice?</returns>
-        public override string getClassHash() {
-            return classHash;
+        public GameField() {
+        }
+
+        // The classHash, a unique identifying string for the class. Hmm, wow, that's kind of redundant, isn't that? C# already provides such a function through reflection. Oh well.
+        // Used for the factory methods called when the client receives a CREATE_NEW_OBJECT update from the server computer.
+        public const string realHash = "GameField";
+        public override string classHash {
+            get { return realHash; }
         }
 
         /// <summary>
         /// The static constructor. Adds the class's factory method to the GameObject factoryList when the class is first loaded.
         /// </summary>
         static GameField() {
-            GameObject.factoryList.Add(classHash, new InteractionEngine.Constructs.GameObjectFactory(makeGameField));
+            GameObject.factoryList.Add(realHash, new InteractionEngine.Constructs.GameObjectFactory(GameObject.createFromUpdate<GameField>));
         }
-        
-        /// <summary>
-        /// A factory method that creates and returns a new instance of this GameObject. Used by the client when the server requests it to make a new GameObject.
-        /// </summary>
-        /// <param name="loadRegion">The LoadRegion to which this GameObject belongs.</param>
-        /// <param name="id">This GameObject's ID.</param>
-        /// <returns>A new instance of this LoadRegion.</returns>
-        static GameField makeGameField(InteractionEngine.Constructs.LoadRegion loadRegion, int id) {
-            if (InteractionEngine.Engine.status != InteractionEngine.Engine.Status.MULTIPLAYER_CLIENT)
-                throw new System.Exception("You're not a client, so why are you calling the GameObject factory method?");
-            GameField field = new GameField(loadRegion, id);
-            return field;
-        }
-
-        /// <summary>
-        /// This method is meant to be called by a subclass constructor, which in turn was called by a factory method on that subclass.
-        /// It instantiates a GameObject from the information that was sent to a MULTIPLAYER_CLIENT by a CREATE_OBJECT packet.
-        /// </summary>
-        /// <param name="loadRegion">The LoadRegion this GameObject belongs to.</param>
-        /// <param name="id">The ID of this GameObject.</param>
-        private GameField(LoadRegion loadRegion, int id) 
-            : base(loadRegion, id) {
-            makeFields();
-        }
-
-        #endregion
-
-        #region GameWorld
-
-        /// <summary>
-        /// All GameObjects need a parameterless constructor for calling by GameObject.createGameObject().
-        /// NEVER CALL THIS! This constructor is exclusively for use by GameObject.createGameObject(). If anyone else calls it things will break.
-        /// </summary>
-        public GameField() {
-        }
-
-        /// <summary>
-        /// Constructs the GameObject from the GameWorld.
-        /// </summary>
-        /// <param name="loadRegion">The LoadRegion to which this GameObject belongs.</param>
-        /// <returns>The constructed GameObject.</returns>
-        public static GameField createGameField(LoadRegion loadRegion) {
-            GameField newField = GameObject.createGameObject<GameField>(loadRegion);
-            if (newField == null) return newField;
-            newField.makeFields();
-            return newField;
-        }
-
-        #endregion
 
         #endregion
 
