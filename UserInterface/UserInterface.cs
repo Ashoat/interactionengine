b5c27cd8966863471c3ec290b6f0c10454de15ef
ashoat <ashoat@d5ddfdca-4257-11de-952f-710615d9d0d5>
2009-05-20 17:06:39 +0000

    Significantly simplified required constructor code in GameObject subclasses by adding generic methods to GameObject.

--- UserInterface/UserInterface.cs
+++ UserInterface/UserInterface.cs
@@ -106,51 +106,33 @@ namespace InteractionEngine.UserInterface {
     /// </summary>
     public class KeyboardFocus : InteractionEngine.Constructs.GameObject {
 
-        #region FACTORY
+        #region Constructors
+
+        /// <summary>
+        /// This helper method creates all of this GameObject's fields in constant order.
+        /// </summary>
+        public override void makeFields() {
+        }
+
+        /// <summary>
+        /// All GameObjects need a parameterless constructor for calling by GameObject.createGameObject() and GameObject.createFromUpdate().
+        /// NEVER CALL THIS! This constructor is exclusively for use by the InteractionEngine. If anyone else calls it things will break.
+        /// </summary>
+        public KeyboardFocus() {
+        }
 
         // The classHash, a unique identifying string for the class. Hmm, wow, that's kind of redundant, isn't that? C# already provides such a function through reflection. Oh well.
         // Used for the factory methods called when the client receives a CREATE_NEW_OBJECT update from the server computer.
-        internal const string classHash = "KeyboardFocus";
+        public const string realHash = "KeyboardFocus";
+        public override string classHash {
+            get { return realHash; }
+        }
 
         /// <summary>
         /// The static constructor. Adds the class's factory method to the GameObject factoryList when the class is first loaded.
         /// </summary>
         static KeyboardFocus() {
-            InteractionEngine.Constructs.GameObject.factoryList.Add(classHash, new InteractionEngine.Constructs.GameObjectFactory(makeKeyboardFocus));
-        }
-
-        /// <summary>
-        /// A factory method that creates and returns a new instance of KeyboardFocus. Used by the client when the server requests it to make a new GameObject.
-        /// </summary>
-        /// <param name="loadRegion">The LoadRegion to which this GameObject belongs.</param>
-        /// <param name="id">This GameObject's ID.</param>
-        /// <param name="reader">The PacketReader from which we will read the fields of the newly constructed GameObject.</param>
-        /// <returns>A new instance of KeyboardFocus.</returns>
-        static KeyboardFocus makeKeyboardFocus(InteractionEngine.Constructs.LoadRegion loadRegion, int id) {
-            if (InteractionEngine.Engine.status != InteractionEngine.Engine.Status.MULTIPLAYER_CLIENT)
-                throw new System.Exception("You're not a client, so why are you calling the GameObject factory method?");
-            KeyboardFocus keyboardFocus = new KeyboardFocus(loadRegion, id);
-            // ORDER OF STUFF (where you used the reader to construct datatypes, used factory methods exclusively. also, construct modules and their datatypes here too.)
-            return keyboardFocus;
-        }
-
-        /// <summary>
-        /// Constructs a GameObject and assigns it an ID.
-        /// This is the constructor that should be used if and only if you are a MULTIPLAYER_CLIENT.
-        /// Furthermore, it is only called by the GameObjectFactory method.
-        /// </summary>
-        /// <param name="loadRegion">The LoadRegion to which this GameObject belongs.</param>
-        /// <param name="id">This GameObject's ID.</param>
-        private KeyboardFocus(InteractionEngine.Constructs.LoadRegion loadRegion, int id)
-            : base(loadRegion, id) {
-        }
-
-        /// <summary>
-        /// Returns the class hash. 
-        /// </summary>
-        /// <returns>The class hash. Do we really have to tell you everything twice?</returns>
-        public override string getClassHash() {
-            return classHash;
+            Constructs.GameObject.factoryList.Add(realHash, new InteractionEngine.Constructs.GameObjectFactory(Constructs.GameObject.createFromUpdate<KeyboardFocus>));
         }
 
         #endregion
