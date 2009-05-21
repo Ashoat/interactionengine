9af74fd07da92130507b3999393ef37ff139c8e8
ashoat <ashoat@d5ddfdca-4257-11de-952f-710615d9d0d5>
2009-05-21 22:16:58 +0000

    more updates to the TestNetworkGame. server-side is now done.

--- TestNetworkGame/MAIN.cs
+++ TestNetworkGame/MAIN.cs
@@ -17,28 +17,10 @@ namespace TestNetworkGame {
             // Set up the UI.
             Engine.userInterface = new UserInterface2D();
             ((UserInterface2D)Engine.userInterface).setWindowSize(430, 430);
-            // Set up the LoadRegion.
-            LoadRegion loadRegion = LoadRegion.createLoadRegion();
-            // Set up the GameField.
-            GameField gameField = GameObject.createGameObject<GameField>(loadRegion);
-            const int offset = 56;
-            const int padding = 108;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    // Give us an O!
-                    O firstO = GameObject.createGameObject<O>(loadRegion);
-                    firstO.setPosition(offset + padding * i, offset + padding * j);
-                    firstO.getGamePiece().display.value = false;
-                    // Give us an X!
-                    X firstX = GameObject.createGameObject<X>(loadRegion);
-                    firstX.setPosition(offset + padding * i, offset + padding * j);
-                    firstX.getGamePiece().display.value = false;
-                    // Give us a ClickySpot!
-                    ClickySpot firstClicky = GameObject.createGameObject<ClickySpot>(loadRegion);
-                    firstClicky.setPosition(offset + padding * i, offset + padding * j);
-                    firstClicky.relateToGamePieces(firstO, firstX);
-                }
-            }
+            // Set up the local LoadRegion with its buttons.
+            LoadRegion localRegion = LoadRegion.createLoadRegion();
+            Host hostButton = GameObject.createGameObject<Host>(localRegion);
+            hostButton.setPosition(50, 400);
             // Go!
             Engine.game.Run();
         }
