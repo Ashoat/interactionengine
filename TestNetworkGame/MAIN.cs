3feb1b89a12b73fa55b6134185604de46048433d
ashoat <ashoat@d5ddfdca-4257-11de-952f-710615d9d0d5>
2009-05-22 03:13:41 +0000

    it's straight-up working, bitches.

--- TestNetworkGame/MAIN.cs
+++ TestNetworkGame/MAIN.cs
@@ -21,6 +21,15 @@ namespace TestNetworkGame {
             LoadRegion localRegion = LoadRegion.createLoadRegion();
             Host hostButton = GameObject.createGameObject<Host>(localRegion);
             hostButton.setPosition(50, 400);
+            Join joinButton = GameObject.createGameObject<Join>(localRegion);
+            joinButton.setPosition(350, 400);
+            hostButton.associateJoin(joinButton);
+            joinButton.associateHost(hostButton);
+            // Annoyance: Static constructors aren't called unless I bother the class first. Ugh.
+            new GameField();
+            new ClickySpot();
+            new O();
+            new X();
             // Go!
             Engine.game.Run();
         }
