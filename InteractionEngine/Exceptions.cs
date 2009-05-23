408a6291445765fa0d20db58c07a6a30c8d291c9
ashoat <ashoat@d5ddfdca-4257-11de-952f-710615d9d0d5>
2009-05-23 00:09:40 +0000

    Exceptions!

---
+++ InteractionEngine/Exceptions.cs
@@ -0,0 +1,70 @@
+﻿/*••••••••••••••••••••••••••••••••••••••••*\
+| Interaction Engine                       |
+| (C) Copyright Bluestone Coding 2008-2009 |
+|••••••••••••••••••••••••••••••••••••••••••|
+|           __    ___ ___  ___             |
+|          /++\  | _ ) __|/ __|            |
+|          \++/  | _ \__ \ (__             |
+|           \/   |___/___/\___|            |
+|                                          |
+|••••••••••••••••••••••••••••••••••••••••••|
+| EXCEPTION HANDLING                       |
+| * InteractionEngineException       Class |
+| * GameWorldException               Class |
+\*••••••••••••••••••••••••••••••••••••••••*/
+
+namespace InteractionEngine {
+
+    /**
+     * This class represents an exception caused by an error within the InteractionEngine.
+     */
+    public class InteractionEngineException : System.Exception {
+
+        /// <summary>
+        /// Construct an InteractionEngineException.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public InteractionEngineException(string message)
+            : base(message) {
+
+        }
+
+        /// <summary>
+        /// Construct an InteractionEngineException.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="innerException">The Exception that caused this InteractionEngineException to be thrown.</param>
+        public InteractionEngineException(string message, System.Exception innerException)
+            : base(message, innerException) {
+
+        }
+
+    }
+
+    /**
+     * This class represents an exception caused by an error within the InteractionEngine.
+     */
+    public class GameWorldException : System.Exception {
+
+        /// <summary>
+        /// Construct an InteractionEngineException.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public GameWorldException(string message)
+            : base(message) {
+
+        }
+
+        /// <summary>
+        /// Construct an InteractionEngineException.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="innerException">The Exception that caused this GameWorldException to be thrown.</param>
+        public GameWorldException(string message, System.Exception innerException)
+            : base(message, innerException) {
+
+        }
+
+    }
+
+}
