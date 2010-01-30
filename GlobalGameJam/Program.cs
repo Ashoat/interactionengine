9206cba10f1b012c611f5fcc710b05491c89f848
myncknm@yahoo.com <myncknm@yahoo.com@d5ddfdca-4257-11de-952f-710615d9d0d5>
2010-01-30 05:23:55 +0000

    Added basic game-starting code.

--- GlobalGameJam/Program.cs
+++ GlobalGameJam/Program.cs
@@ -1,19 +1,33 @@
 using System;
+using InteractionEngine.UserInterface.TwoDimensional;
+using InteractionEngine.Constructs;
+using InteractionEngine;
 
-namespace GlobalGameJam
-{
-    static class Program
-    {
+namespace GlobalGameJam {
+
+    static class Program {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main(string[] args)
-        {
-            using (Game1 game = new Game1())
-            {
-                game.Run();
-            }
-        }
-    }
-}
+        static void Main(string[] args) {
+            // Set up the UI.
+            Engine.userInterface = new UserInterface2D();
+            ((UserInterface2D)Engine.userInterface).setWindowSize(430, 430);
 
+            // Set up the engine
+            Engine.status = Engine.Status.SINGLE_PLAYER;
+
+            // Set up the local LoadRegion with its buttons.
+            LoadRegion localRegion = LoadRegion.createLoadRegion();
+
+            // Annoyance: Static constructors aren't called unless I bother the class first. Ugh.
+
+            // Go!
+            Engine.run();
+        }
+
+    }
+
+
+}
